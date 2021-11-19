e938ce71ee1bfbda4b0d6984f64e2948e97e4907
Marcos Peter <mpeterlobato@gmail.com>
2021-11-19 10:53:11 -0300

    Talking about my hobbies using some JS

---
+++ front-end-lovelace/modulo07-parte07/scripts/script.js
@@ -0,0 +1,16 @@
+document.getElementById('name').innerHTML = "Marcos Peter Souza Lobato Junior";
+document.getElementById('age').innerHTML = "27 years old";
+document.getElementById('state').innerHTML = "Distrito Federal";
+document.getElementById('pic').src = "assets/marcos-peter.jpg";
+document.getElementById('pic').alt = "Marcos Peter";
+
+document.getElementById('hobby-title').innerHTML = "Music, books and movies!";
+document.getElementById('hobby-description').innerHTML = "As long as I can remember I love listening to music and watching movies! One of my favorite things to do is spend time alone with my thoughts listening to music or watching a movie. And with time, as I matured, I developed a taste for reading. I'm fascinated by the fantasy and horror genre and like to take time out of my day to read whenever I can! I can say for sure that Led Zeppelin and J. R. R. Tolkien are my biggest hobbies!";
+document.getElementById('lz').innerHTML = "Led Zeppelin";
+document.getElementById('lz-link').href = "https://www.ledzeppelin.com/?frontpage=true";
+document.getElementById('lz-link').target = "_blank";
+document.getElementById('lz-img').src = "assets/led-zeppelin.jpg";
+document.getElementById('tolkien').innerHTML = "J. R. R. Tolkien";
+document.getElementById('tolkien-link').href = "https://pt.wikipedia.org/wiki/J._R._R._Tolkien";
+document.getElementById('tolkien-link').target = "_blank";
+document.getElementById('tolkien-img').src = "assets/tolkien.jpg";
