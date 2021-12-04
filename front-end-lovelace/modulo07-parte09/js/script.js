5f8a4519964fcb92fe13caf743a726aa3ae5e4ea
Marcos Peter <mpeterlobato@gmail.com>
2021-12-04 00:33:06 -0300

    Updating some changes in the main directory

---
+++ front-end-lovelace/modulo07-parte09/js/script.js
@@ -0,0 +1,17 @@
+//variables
+const fillMainSectionElement = document.getElementById("section");
+const fillMainArticleElement = document.getElementById("article");
+
+//functions
+function fillMainSectionContent(){
+    fillMainSectionElement.innerHTML = 
+    "<h1>Lobo-Cinzento</h1><img src='assets/lobocinzento-0.webp' alt='lobo'>";
+}
+
+function fillMainArticleContent(){
+    fillMainArticleElement.innerHTML = 
+    "<h2>Lobo-Cinzento</h2><p>Os lobos tendem a apresentar maior tamanho nas regiões mais frias do planeta, e vão diminuindo em locais de temperaturas mais amenas.Os animais encontrados na Europa chegam a atingir até 38,5 kg, enquanto os da América do Norte até 35 kg, os menores são os que habitam a Índia e atingem até 25 kg. As fêmeas sempre apresentam menor porte.O pelo que reveste o corpo tem papel importante, garantindo o seu isolamento térmico e a sobrevivência em ambientais mais frios.A coloração do pelo pode variar bastante, indo de tons de acinzentados, branco, vermelho, marrom e até preto. Cabe ressaltar que a coloração auxilia na camuflagem.</p><h2>Hábitos</h2><p>Os lobos vivem em grupos, as alcateias, liderada por um macho alfa, o qual apresentar maior força e poder de caça comparado aos outros membros do grupo.O macho alfa forma casal com a fêmea alfa, responsável pelas fêmeas da alcateia. Eles formam um casal monogâmico que vivem juntos durante toda a vida e lideram o grupo.Quando um macho alfa morre, a liderança do grupo passa para o macho beta, o segundo na hierarquia de organização da alcateia.Em geral, os lobos são inteligentes, velozes e estrategistas no momento da caça, sendo reconhecidos como excelentes predadores.Eles também são animais territorialistas e disputam o poder do espaço com grupos rivais. É comum a delimitação das fronteiras entre os grupos com fezes e urina.</p><img src='assets/lobodoartico.jpg' /><h2>Descrição</h2><p>Os lobos são animais mamíferos da família Canidae, sendo o representante selvagem da família que também inclui a raposa e o cachorro.Existem três espécies de lobos: o lobo-cinzento (Canis lupus), o lobo-etíope (Canis simensis) e o lobo-vermelho (Canis rufus), além de muitas subespécies. Só de Canis lupus existem 31 subespécies.</p><a href='https://pt.wikipedia.org/wiki/Lobo'>Saiba mais clicando aqui</a>";
+}
+
+fillMainSectionContent();
+fillMainArticleContent();
